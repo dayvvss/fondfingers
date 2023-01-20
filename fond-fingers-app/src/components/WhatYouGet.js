fb039ddd8a02535478e83abe5a34cf1ed1128f8a
Dave <dave@MacBook-Pro.local>
2023-01-20 12:03:27 +0300

    Slight change

--- fond-fingers-app/src/components/WhatYouGet.js
+++ fond-fingers-app/src/components/WhatYouGet.js
@@ -16,7 +16,7 @@ export default function WhatYouGet() {
     <Row className='justify-content-center mt-5 p-lg-5 bg-white'>
         <h1 className='text-dark w-auto mb-lg-4'>What You Get</h1>
         <Container  className='d-flex justify-content-center px-2 mx-0 flex-lg-row flex-md-column flex-column my-md-4 my-lg-5'>
-            <Col xs={10} md={4} lg={4} className='ms-4 ms-md-0 ms-lg-0 mx-md-3 scale-on-hover'>
+            <Col xs={10} md={5} lg={4} className='ms-4 ms-md-0 ms-lg-0 mx-md-3 scale-on-hover'>
                 <Card className='bg-transparent p-3 m-lg-4 my-3   card-height  justify-content-center outline-brown text-dark'>
                     <div className="text-center mb-3"><LibraryMusicIcon className='large-icon' /></div>
                     
