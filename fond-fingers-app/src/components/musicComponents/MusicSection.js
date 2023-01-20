d721311b149d04e47efea9158911419fb3f9e8a3
Dave <dave@MacBook-Pro.local>
2023-01-20 03:48:55 +0300

    improvements on metadata objects

--- fond-fingers-app/src/components/musicComponents/MusicSection.js
+++ fond-fingers-app/src/components/musicComponents/MusicSection.js
@@ -1,65 +1,12 @@
 import {useState,useEffect} from 'react'
 import Player from './Player';
 import Row from 'react-bootstrap/Row'
+import { genres,vocals,moods,tempos } from './songData';
 import Container from 'react-bootstrap/Container'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MusicSection() {
-  const genres = {
-        1:"Pop",
-        2:"Amapiano",
-        3:"Hip-hop",
-        4:"Electronic",
-        5:"Jazz",
-        6:"Blues",
-        7:"Folk",
-        8:"African",
-        9:"R&B/Soul",
-        10:"Latin",
-        11:"Classical",
-        12:"Reggae",
-        13:"Funk",
-        14:"Metal",
-        15:"Punk",
-        16:"Gospel/Christian",
-        17:"Grunge",
-        18:"Bluegrass",
-        19:"World Music"
-  }
-  // Vocals
-const vocals =
-{
-    1:"male",
-    2:"female",
-    3:"no-vocals" 
-}
-// Moods
-const moods ={
-        1:"Upbeat",
-        2:"Mellow",
-        3:"Sad",
-        4:"Happy",
-        5:"Energetic",
-        6:"Romantic",
-        7:"Dramatic",
-        8:"Atmospheric",
-        9:"Intense",
-        10:"Relaxing",
-        11:"Funky",
-        12:"Sensual",
-        13:"Mysterious",
-        14:"Suspenseful",
-        15:"Dreamy"
 
-}
- const tempos = {
-  // Song Tempos
-      Fast: "130-160 BPM",
-      Medium: "90-120 BPM",
-      Slow: "60-80 BPM"
-  
-
- } 
   const [songs,setSongs] = useState([
     {
       title:"Song 1",
